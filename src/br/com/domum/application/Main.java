52dca96f1747c7affb1d413a85a71d87e75a1c57
Diego Barros <diegorbj@outlook.com>
2021-03-19 20:02:21 -0300

    Versão inical.

---
+++ src/br/com/domum/application/Main.java
@@ -0,0 +1,48 @@
+package br.com.domum.application;
+
+import br.com.domum.entities.Employees;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String fullFilePath = sc.nextLine();
+
+        List<Employees> employees = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fullFilePath))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                employees.add(new Employees(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.print("Enter Salary: ");
+        Double salary = sc.nextDouble();
+
+        System.out.printf("Email of people whose salary is more than %.2f:%n", salary);
+
+        List<String> emails = employees.stream()
+                .filter(e -> e.getSalary() > salary)
+                .map(Employees::getEmail)
+                .sorted()
+                .collect(Collectors.toList());
+
+        emails.forEach(System.out::println);
+
+        sc.close();
+    }
+}
